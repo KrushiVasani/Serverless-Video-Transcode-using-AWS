ad2942d50b2e4232e4bfea3484d01a2145b8784b
KrushiVasani <56724470+KrushiVasani@users.noreply.github.com>
2021-03-19 21:36:56 +0530

    commit

---
+++ website/js/config.js
@@ -0,0 +1,7 @@
+var configConstants = {
+    auth0: {
+        domain: 'serverlessvideotranscode.us.auth0.com',
+        clientId: 'SK2ucqxW4RWlUr67Ji3jZM7p3pAqAZM6'
+    },
+	apiBaseUrl:'https://5109yaeu6i.execute-api.us-east-1.amazonaws.com/dev'
+};
